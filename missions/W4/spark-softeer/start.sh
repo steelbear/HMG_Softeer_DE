058162cce55cdef284739b70febde7b2b7ba17a5
steelbear <choisteelbear@gmail.com>
2024-07-26 10:20:12 +0900

    W4M1 - edit start.sh to start connect server

--- missions/W4/spark-softeer/start.sh
+++ missions/W4/spark-softeer/start.sh
@@ -3,6 +3,7 @@
 
 if [ $1 = "master" ]; then
     LOG_FILE=$(./sbin/start-master.sh | sed "s/.\+logging to //")
+    ./sbin/start-connect-server.sh
 elif [ $1 = "worker" ]; then
     LOG_FILE=$(./sbin/start-worker.sh spark://master:7077 | sed "s/.\+logging to //")
 else
