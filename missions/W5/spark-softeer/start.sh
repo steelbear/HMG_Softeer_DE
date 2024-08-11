dda47595f90cae6338cbd057c6aa38908313a81c
steelbear <choisteelbear@gmail.com>
2024-08-11 23:15:22 +0900

    W5M1 - Data Analysis using RDD

---
+++ missions/W5/spark-softeer/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ $1 = "master" ]; then
+    LOG_FILE=$(./sbin/start-master.sh | sed "s/.\+logging to //")
+elif [ $1 = "connect" ]; then
+    LOG_FILE=$(./sbin/start-connect-server.sh --packages org.apache.spark:spark-connect_2.12:3.5.1 --master spark://master:7077 | sed "s/.\+logging to //")
+elif [ $1 = "worker" ]; then
+    LOG_FILE=$(./sbin/start-worker.sh spark://master:7077 | sed "s/.\+logging to //")
+else
+    echo "Usage: $0 [master|connect|worker]"
+    exit 1
+fi 
+
+tail -f $LOG_FILE
